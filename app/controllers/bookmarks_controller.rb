cb240566d97794de11f98b18a3419df36888dbaf
Soojin Hwang <soojiny.hwang@gmail.com>
2022-04-30 16:00:15 +0100

    contollers and views created

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,6 +8,7 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
     @bookmark.list = @list
+
     if @bookmark.save
       redirect_to list_path(@list)
     else
@@ -25,6 +26,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
