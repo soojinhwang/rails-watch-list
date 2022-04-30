0eca107ea37efe9cbbf86e7c006a066f84f4ceb6
Soojin Hwang <soojiny.hwang@gmail.com>
2022-04-30 14:14:11 +0100

    lists and booksmarks controllers programmed, associated views created

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,19 +1,18 @@
 class BookmarksController < ApplicationController
   def new
     @list = List.find(params[:list_id])
-    @movie = Movie.find(params[:movie_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
-    @movie = Movie.find(params[:movie_id])
     @bookmark.list = @list
-    @bookmark.movie = @movie
-    @bookmark.save
-
-    redirect_to list_path(@list)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -26,6 +25,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 end
